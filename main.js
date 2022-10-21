ae2312775d7ffcb4874ed6dd20622545a35eb84e
kenny0619 <kehindeibukun0619@gmail.com>
2022-10-21 19:33:16 +0100

    feat: adds send message functions

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const jayson = require("jayson");
 const Web3 = require("web3");
 const client = jayson.client.tcp({
@@ -6,41 +7,136 @@ const client = jayson.client.tcp({
   version: 1,
 });
 
+const sender = {
+  pubKey: process.env.PUBLIC_KEY,
+  privKey: process.env.PRIVATE_KEY,
+};
+
 const web3 = new Web3();
 
+let channelName = "ioc-committee",
+  subscriber = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  timestampSub = Math.floor(Number(Date.now().toString()) / 1000),
+  actionSub = "join";
 
-const channel = "Channel One";
-const subscriber = "0x5c7983dd79A4461Bc2e9AeAdD9364a41D49A64dc";
-const sender = "0x5c7983dd79A4461Bc2e9AeAdD9364a41D49A64dc";
-const timestamp = Math.floor(Date.now()/1000);
-const action = "join";
-
+const { signature: channelSignature } = web3.eth.accounts.sign(
+  web3.utils.soliditySha3(channelName.toLowerCase()),
+  sender.privKey
+);
 let sub = [];
-sub.push(`Channel:${channel}`);
-sub.push(`Timestamp:${timestamp}`);
-sub.push(`Action:${action}`);
+sub.push(`Channel:${channelSignature}`);
+sub.push(`ChannelName:${channelName}`);
+sub.push(`Timestamp:${timestampSub}`);
+sub.push(`Action:${actionSub}`);
+sub = sub.join(",");
+const { signature: signSub } = web3.eth.accounts.sign(
+  web3.utils.soliditySha3(sub),
+  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+);
 
-const message = sub.join(",");
-const { signature } = web3.eth.accounts.sign(
-    web3.utils.soliditySha3(message),
-    "73fa35d75a5191e0d29cc260cb0879bc32f7fd3608492e5d5a3e061b48b822c6"
-  );
-
-
+// sign channel name
+console.log("sub:::", sub);
+console.log("signSub:::", signSub);
 
 const subscription = [
   {
-    channel,
+    channel: channelSignature,
+    channelName,
     subscriber,
-    sender,
-    timestamp,
-    signature,
-    action,
-    // action: "leave", "join"
+    timestamp: timestampSub,
+    signature: signSub,
+    action: actionSub,
+    // action: "leave"
   },
 ];
 client.request("RpcService.Subscription", subscription, (err, response) => {
   if (err) throw err;
-  console.log('response', response)
+  console.log("response", response);
   if (response.error) throw response.error;
+});
+
+const timestamp = Date.now().toString();
+// const from = sender.pubKey;
+const receiver = `${channelName}:${channelSignature}`;
+const platform = "channel";
+const type = "html";
+const message = "hello world";
+const chainId = "1";
+const subject = "Test Subject";
+const contract = "Contract";
+const abi = "Abi";
+const action = "Action";
+const parameters = ["good", "Jon", "Doe"];
+const actions = [{ contract, abi, action, parameters }];
+const origin = "0x59fD8f94dDd1Fe6066d300F74afD5E3a01970e43";
+const text = message;
+const html = message;
+
+// user should calculate the hash... propagate it to part where it is stored on the ipfs network...
+// libraries
+// propagating with md5 ...
+
+let chatMessage = [];
+// chatMessage.push(`Header.Sender:${from}`);
+chatMessage.push(`Header.Receiver:${receiver}`);
+chatMessage.push(`Header.ChainId:${chainId}`);
+chatMessage.push(`Header.Platform:${platform}`);
+chatMessage.push(`Header.Timestamp:${timestamp}`);
+chatMessage.push(
+  `Body.Subject:${web3.utils.soliditySha3(subject).toLowerCase()}`
+);
+chatMessage.push(`Body.Message:${web3.utils.soliditySha3(text).toLowerCase()}`);
+
+let _action = [];
+let i = 0;
+while (i < actions.length) {
+  _action.push(`Actions[${i}].Contract:${actions[i].contract}`);
+  _action.push(`Actions[${i}].Abi:${actions[i].abi}`);
+  _action.push(`Actions[${i}].Action:${actions[i].action}`);
+  let _parameter = [];
+  let j = 0;
+  while (j < actions[i].parameters.length) {
+    _parameter.push(
+      `Actions[${i}].Parameters[${j}]:${actions[i].parameters[j]}`
+    );
+    j++;
+  }
+  _parameter = _parameter.join(" ");
+  _action.push(`Actions[${i}].Parameters:[${_parameter}]`);
+  i++;
+}
+_action = _action.join(" ");
+chatMessage.push(`Actions:[${_action}]`);
+chatMessage.push(`Origin:${origin}`);
+chatMessage = chatMessage.join(",");
+console.log("chatMessage:::", chatMessage);
+// params.push(`Actions.Sender:${from}`)
+// params.push(`Body.Sender:${from}`)
+const { signature: messageSignature } = web3.eth.accounts.sign(
+  web3.utils.soliditySha3(chatMessage),
+  sender.privKey
+  //   "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+);
+
+const params = [
+  {
+    timestamp: Number(timestamp),
+    // from,
+    receiver,
+    platform,
+    chainId,
+    type,
+    message,
+    subject,
+    signature: messageSignature,
+    actions,
+    origin,
+  },
+];
+
+client.request("RpcService.SendMessage", params, (err, response) => {
+  console.log("response:::", response);
+  if (err) throw err;
+  if (response.error) throw response.error;
+  console.log("response:::", response);
 });
