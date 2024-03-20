6cfa973351e2a1cdc6e08f052fbd7e498e4746f5
dephizee <hafiz227@gmail.com>
2024-03-20 18:37:28 +0100

    feat: minor

--- dist/src/entities/base.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-export type AddressString = string;
-export type HexString = string;
-export declare class BaseEntity {
-    asPayload(): unknown;
-    encodeBytes(): Buffer;
-    getHash(): Buffer;
-}
